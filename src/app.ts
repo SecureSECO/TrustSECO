6b16260e20cd4743f48ed0251c7e8b21c42cc229
Gijs Blanken <blankengijs@gmail.com>
2022-06-21 09:14:00 +0200

    websocket back

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ import setup from './keys';
 
 setup();
 
-const app = new Koa();
+const app = websockify(new Koa());
 
 app.use(cors()).use(koaBody());
 
@@ -18,7 +18,7 @@ app.use(cors()).use(koaBody());
 app.use(apiRouter.routes()).use(apiRouter.allowedMethods());
 
 // Websocket routes
-// app.ws.use(websocketRouter.routes()).use(websocketRouter.allowedMethods());
+app.ws.use(websocketRouter.routes()).use(websocketRouter.allowedMethods());
 
 // Portal serving
 app.use(serve('./public'));
